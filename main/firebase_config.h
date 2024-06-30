e5af73ab31f3e28279fa2964100da6cb5c24bd8c
Ashish Saini <47918474+ashus3868@users.noreply.github.com>
2024-06-30 12:28:50 +0530

    Update firebase_config.h

--- main/firebase_config.h
+++ main/firebase_config.h
@@ -1,13 +1,13 @@
 // Wifi Credentials
-#define SSID "#include<iot.h>"
-#define PASSWORD "Gruh@@2020"
+#define SSID "EXAMPLE_SSID"
+#define PASSWORD "EXAMPLE_PASSWORD"
 
 // Read readme.md to properly configure api key and authentication
 
 // create a new api key and add it here 
-#define API_KEY "BJZyIz3vaVD4HO6IKXNEc8TcbU00ZsncsFXohfEw"
+#define API_KEY "YOUR-API-KEY-HERE"
 // Copy your firebase real time database link here 
-#define DATABASE_URL "https://esp-idf-fire-default-rtdb.firebaseio.com/"  
+#define DATABASE_URL "https://[PROJECT-NAME-HERE].firebaseio.com/"  
 
-#define USER_EMAIL "innovateyourself2build@gmail.com"   // This gmail does not exist outside your database. it only exists in the firebase project as a user
-#define USER_PASSWORD "12345678"      // Dont add your gmail credentials. Setup users authentication in your Firebase project first
+#define USER_EMAIL "USERNAME"   // This gmail does not exist outside your database. it only exists in the firebase project as a user
+#define USER_PASSWORD "PASSWORD"      // Dont add your gmail credentials. Setup users authentication in your Firebase project first
